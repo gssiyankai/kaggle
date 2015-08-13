d70f7e65dd4cb2f1f05dac2d00273de1e2759042
gssiyankai <syk.greg@gmail.com>
2015-08-13 17:18:40 +0200

    Predict missing age and embarkment

--- titanic/titanic.R
+++ titanic/titanic.R
@@ -1,5 +1,6 @@
 # Load in the package
 library(randomForest)
+library(rpart)
 
 # Load train set and test set
 train <- read.csv("train.csv", header = TRUE)
@@ -8,8 +9,19 @@ test <- read.csv("test.csv", header = TRUE)
 str(train)
 str(test)
 
+# Passenger on row 62 and 830 do not have a value for embarkment.
+# Since many passengers embarked at Southampton, we give them the value S.
+train[train$Embarked=="",]
+train$Embarked[c(62,830)] = "S"
+
+# Predict missing passenger ages
+predicted_age <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked,
+                       data=train[!is.na(train$Age),], method="anova")
+train$Age[is.na(train$Age)] <- predict(predicted_age, train[is.na(train$Age),])
+
+
 # Apply the Random Forest Algorithm
-forest <- randomForest(as.factor(Survived) ~ Sex,
+forest <- randomForest(as.factor(Survived) ~ Pclass + Sex + Age+ SibSp + Parch + Fare + Embarked,
                        data=train, importance=TRUE, ntree=1000)
 
 # Make prediction using the test set
